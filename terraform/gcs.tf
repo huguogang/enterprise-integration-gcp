8e0410d94decd7a92bd984bbf7b7393f9c61d624
huguogang <huguogang@users.noreply.github.com>
2019-09-29 22:06:41 -0700

    Minor format update.

--- terraform/gcs.tf
+++ terraform/gcs.tf
@@ -1,3 +1,9 @@
+###############################################
+# Bucket names reside in a single Cloud Storage namespace. When use this script
+# for another project, make sure the bucket names are updated to something unique.
+# Recommended to add project ID as prefix.
+###############################################
+
 # "inbox" bucket holds uploaded data files.
 resource "google_storage_bucket" "inbox" {
   name     = "developertips-ei-inbox"
@@ -5,14 +11,15 @@ resource "google_storage_bucket" "inbox" {
 
   force_destroy = false
   storage_class = "REGIONAL"
-  
+
   lifecycle_rule {
-      action {
-          type = "Delete"
-      }
-      condition {
-          age = 100
-      }
+    action {
+      type = "Delete"
+    }
+
+    condition {
+      age = 100
+    }
   }
 }
 
@@ -23,14 +30,15 @@ resource "google_storage_bucket" "error" {
 
   force_destroy = false
   storage_class = "REGIONAL"
-  
+
   lifecycle_rule {
-      action {
-          type = "Delete"
-      }
-      condition {
-          age = 1000
-      }
+    action {
+      type = "Delete"
+    }
+
+    condition {
+      age = 1000
+    }
   }
 }
 
@@ -41,107 +49,115 @@ resource "google_storage_bucket" "archive" {
 
   force_destroy = false
   storage_class = "REGIONAL"
-  
+
   # Archive file can progress to near line, cold line then deleted
   lifecycle_rule {
-      action {
-          type = "SetStorageClass"
-          storage_class = "COLDLINE"
-      }
-      condition {
-          age = 100
-      }
+    action {
+      type          = "SetStorageClass"
+      storage_class = "COLDLINE"
+    }
+
+    condition {
+      age = 100
+    }
   }
 
   lifecycle_rule {
-      action {
-          type = "Delete"
-      }
-      condition {
-          age = 200
-      }
+    action {
+      type = "Delete"
+    }
+
+    condition {
+      age = 200
+    }
   }
 }
 
 #############################################
-# Setup Cloud Pub/Sub Change Notification for inbox bucket.
+# Cloud Pub/Sub Change Notification for inbox bucket.
 #############################################
 resource "google_pubsub_topic" "inbox" {
-    name = "ei-inbox"
+  name = "ei-inbox"
 }
 
 # Special Google Cloud Storage service account.
 data "google_storage_project_service_account" "gcs_account" {}
 
 resource "google_pubsub_topic_iam_binding" "binding" {
-    topic       = "${google_pubsub_topic.inbox.name}"
-    role        = "roles/pubsub.publisher"
-    members     = ["serviceAccount:${data.google_storage_project_service_account.gcs_account.email_address}"]
+  topic   = "${google_pubsub_topic.inbox.name}"
+  role    = "roles/pubsub.publisher"
+  members = ["serviceAccount:${data.google_storage_project_service_account.gcs_account.email_address}"]
 }
 
 resource "google_storage_notification" "notification" {
-    bucket            = "${google_storage_bucket.inbox.name}"
-    payload_format    = "JSON_API_V1"
-    topic             = "${google_pubsub_topic.inbox.name}"
-    # Only need to be notified when a new object is successfully uploaded.
-    event_types       = ["OBJECT_FINALIZE"]
-    depends_on        = ["google_pubsub_topic_iam_binding.binding"]
+  bucket         = "${google_storage_bucket.inbox.name}"
+  payload_format = "JSON_API_V1"
+  topic          = "${google_pubsub_topic.inbox.name}"
+
+  # Only need to be notified when a new object is successfully uploaded.
+  event_types = ["OBJECT_FINALIZE"]
+  depends_on  = ["google_pubsub_topic_iam_binding.binding"]
 }
 
 #############################################
-# A cloud function that is triggered on new files uploaded to inbox.
+# A cloud function that subscribe to the CPS notification. It will be used
+# to demo handling logic.
 #############################################
 resource "google_cloudfunctions_function" "inbox" {
-  name                  = "ei-inbox-function"
-  description           = "Handles new files uploaded to inbox."
-  runtime               = "nodejs10"
-  available_memory_mb   = 128
+  name                = "ei-inbox-function"
+  description         = "Handles new files uploaded to inbox."
+  runtime             = "nodejs10"
+  available_memory_mb = 128
 
   source_archive_bucket = "${google_storage_bucket.ei_source.name}"
   source_archive_object = "${google_storage_bucket_object.ei_inbox_function_zip.name}"
-  
-  entry_point           = "handleInboxNotification"
+
+  entry_point = "handleInboxNotification"
+
   event_trigger {
-      event_type = "google.pubsub.topic.publish"
-      resource = "${google_pubsub_topic.inbox.name}"
-      failure_policy {
-          # Disable Cloud Function's retry. For this integration pipeline, retry is controlled by Cloud Pub/Sub.
-          # As long as Cloud Function do not ACK the message, CPS will keep retrying for about 2 weeks.
-          retry = false
-      }
+    event_type = "google.pubsub.topic.publish"
+    resource   = "${google_pubsub_topic.inbox.name}"
+
+    failure_policy {
+      # Disable Cloud Function's retry. For this integration pipeline, retry is controlled by Cloud Pub/Sub.
+      # As long as Cloud Function do not ACK the message, CPS will keep retrying for about 2 weeks.
+      retry = false
+    }
   }
 }
 
 # Archive cloud function source code in zip file.
 data "archive_file" "ei_inbox_function_zip" {
- type        = "zip"
- source_dir  = "${path.root}/ei-inbox-function/"
- output_path = "${path.root}/depoly/ei-inbox-function.zip"
+  type        = "zip"
+  source_dir  = "${path.root}/ei-inbox-function/"
+  output_path = "${path.root}/depoly/ei-inbox-function.zip"
 }
 
 # Bucket for the source code
 resource "google_storage_bucket" "ei_source" {
-  name   = "developertips-ei-source"
+  name     = "developertips-ei-source"
   location = "us-west2"
 
   force_destroy = false
   storage_class = "REGIONAL"
-  
+
   lifecycle_rule {
-      action {
-          type = "Delete"
-      }
-      condition {
-          age = 1
-      }
+    action {
+      type = "Delete"
+    }
+
+    condition {
+      age = 1
+    }
   }
 }
 
 # Store the cloud function source code in GCS.
 resource "google_storage_bucket_object" "ei_inbox_function_zip" {
- # To ensure Cloud Function picks up new code. File name need to be updated for each new version.
- # This is workaround suggested in: https://github.com/terraform-providers/terraform-provider-google/issues/1938
- name   = "ei-inbox-function.${data.archive_file.ei_inbox_function_zip.output_md5}.zip"
- bucket = "${google_storage_bucket.ei_source.name}"
- source = "${data.archive_file.ei_inbox_function_zip.output_path}"
+  # To ensure Cloud Function picks up new code. File name need to be updated for each new version.
+  # This is workaround suggested in: https://github.com/terraform-providers/terraform-provider-google/issues/1938
+  name = "ei-inbox-function.${data.archive_file.ei_inbox_function_zip.output_md5}.zip"
+
+  bucket = "${google_storage_bucket.ei_source.name}"
+  source = "${data.archive_file.ei_inbox_function_zip.output_path}"
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,5 @@
 provider "google" {
+  # Change to your own project and region / zone preference.
   project = "developertips"
   region  = "us-central1"
   zone    = "us-central1-c"
